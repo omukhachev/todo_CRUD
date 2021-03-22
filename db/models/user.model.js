30d05192ef4877025db9996c6f671b0a7f8d70d2
Oleg Muhachev <olmu1996@gmail.com>
2021-03-22 11:18:35 +0300

    user CRUD functions added

--- db/models/user.model.js
+++ db/models/user.model.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    _id: {type: String, required: true},
     login: {type: String, required: true},
     password: {type: String, required: true},
 });
--- db/routes/user.route.js
+++ db/routes/user.route.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const user_controller = require('../controllers/user.controller');
 
-router.get('/test', user_controller.test);
 router.post('/create', user_controller.user_create);
 router.get('/:id', user_controller.user_get_data);
 router.put('/:id/update', user_controller.user_update);
